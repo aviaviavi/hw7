34f1d80489d59ab34457b25b5d7e2e099c382382
aviaviavi <avipress@gmail.com>
2014-04-30 17:17:08 -0700

    eigenfaces working

--- eigenfaces.m
+++ eigenfaces.m
@@ -1,23 +1,86 @@
 load 'mask.mat'
+mask = mask(:,:,1);
 unmasked_pixels = find(mask);
-cd 'CelebrityDatabase'
-celeb_files = dir('*.jpg')';
+celeb_files = dir('CelebrityDatabase/*.jpg')';
+student_files = dir('StudentDatabase/*.jpg')';
 celebs = [];
+students = [];
+k = 20;
+%process celebrity pictures
 for celeb = celeb_files
-    img = imread(celeb.name);
+    img = rgb2gray(uint8(imread(sprintf('CelebrityDatabase/%s', celeb.name))));
     im_vector = img(unmasked_pixels);
     full_img = zeros(size(mask));
     full_img(unmasked_pixels) = im_vector;
-    full_img = rgb2gray(uint8(full_img));
+    full_img = uint8(full_img);
     imshow((full_img))
-    im_vector = full_img(unmasked_pixels(1:17317));
+    %im_vector = full_img(unmasked_pixels);
     celebs = [celebs; im_vector'];
 end
-cd '..'
+%process student pictures
+for student = student_files
+    img = rgb2gray(uint8(imread(sprintf('StudentDatabase/%s', student.name))));
+    im_vector = img(unmasked_pixels);
+    full_img = zeros(size(mask));
+    full_img(unmasked_pixels) = im_vector;
+    full_img = uint8(full_img);
+    imshow((full_img))
+    %im_vector = full_img(unmasked_pixels);
+    students = [students; im_vector'];
+end
 
 cov_matrix = cov(double(celebs));
 disp 'here'
-[V, D] = eigs(cov_matrix, 10);
+[V, D] = eigs(cov_matrix, k);
+
+
+% for i = 1:10
+%    figure;
+%    V(:,i) = 255*(V(:,i) / max(V(:,i)));
+%    eig = V(:,i);
+%    
+%    
+%    fullimg = zeros(330,280);
+%    fullimg(unmasked_pixels) = eig;
+%    imgtosave = imshow(uint8(fullimg));
+%    saveas(imgtosave, sprintf('eigenface_%d', i), 'png');
+% end
+
+rec_students = [1 2 3 4 5];
+for face = rec_students
+    original = zeros(330, 280);
+    reconstructed = zeros(330, 280);
+    x = V \ double(students(face,:)');
+    new_face = V * x;
+    original(unmasked_pixels) = students(face,:);
+    reconstructed(unmasked_pixels) = new_face;
+    figure;
+    subplot(1, 2, 1), imshow(uint8(original));
+    subplot(1, 2, 2), imshow(uint8((reconstructed)));
+end
+
+total_error = [];
+for num_eigs = 1:k
+    error = 0;
+    for i = 1:size(students, 1)
+        x = V(:,1:num_eigs) \ double(students(i,:))';
+        new_face = V(:,1:num_eigs) * x;
+        error = error + norm(new_face' - double(students(i,:)));
+    end
+    error = error / size(students, 1);
+    total_error = [total_error, error];
+end
+figure;
+plot([1:k], total_error)
+title('Average L2 error vs Number of Eigenfaces (students)');
+
+
+
+
+
+
+
+
 
 
 
--- hw7.m
+++ hw7.m
@@ -4,5 +4,10 @@ test_data = load('data/test.mat');
 train_small = create_sparse_img(double(train_small_data.train{3}.images));
 %train = create_sparse_img(double(train_data.train.images));
 test_images = create_sparse_img(double(test_data.test.images));
+k = 20
+means = kmeans(train_small, k);
 
-means = kmeans(train_small, 20);
+for i = 1:size(means, 1)
+    img = imagesc(reshape(means(i,:), 28, 28));
+    saveas(img, sprintf('kmeans_%d_%d', k, i), 'png');
+end
